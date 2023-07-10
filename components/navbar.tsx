343ffee2fcb723dbeb4476e9671a7b67a4734394
Rohan Tanwar <rohantanwwar0809@gmail.com>
2023-07-10 09:18:50 +0530

    👌 IMPROVE: clerk logout redirect url

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -24,7 +24,7 @@ const Navbar = async () => {
         <MainNav className="mx-6" />
         <div className="ml-auto flex items-center space-x-4">
           <ThemeToggle />
-          <UserButton />
+          <UserButton afterSignOutUrl="/" />
         </div>
       </div>
     </div>
